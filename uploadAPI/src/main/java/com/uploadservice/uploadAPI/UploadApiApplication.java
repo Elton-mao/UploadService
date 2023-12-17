b67f484c4b7187e0cb992d3f8786b09fd4eaec15
Elton Almeida <72717766+Elton-mao@users.noreply.github.com>
2023-12-17 14:37:39 -0300

    start project

---
+++ uploadAPI/src/main/java/com/uploadservice/uploadAPI/UploadApiApplication.java
@@ -0,0 +1,13 @@
+package com.uploadservice.uploadAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadApiApplication.class, args);
+	}
+
+}
---
+++ uploadAPI/src/test/java/com/uploadservice/uploadAPI/UploadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uploadservice.uploadAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
